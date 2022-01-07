b49037c6a91217d1930cf711b6fc644128e6d161
DV1207 <93965485+DV1207@users.noreply.github.com>
2022-01-07 15:10:28 +0530

    Update curation.scala

--- curation.scala
+++ curation.scala
@@ -3,30 +3,33 @@ try
 {
 import com.github.music.of.the.ainur.almaren.builder.Core.Implicit;
 import com.github.music.of.the.ainur.almaren.Almaren;
+import com.modak.common.credential.Credential
 import org.apache.spark.sql.SaveMode
-import com.modak.common.credential.{Credential}
 import com.modak.common._
 
 val almaren = Almaren("Demo")
-val token = sc.getConf.get("spark.nabu.token")
-val api = sc.getConf.get("spark.nabu.fireshots_url")
-
+    
 val args = sc.getConf.get("spark.driver.args").split("\\s+")
-val credentialIdJbdc = args(0).toInt
-val credentialTypeIdJbdc = args(1).toInt
-
-val credentialResult = Credential.getCredentialData(CredentialPayload(s"$token", credentialIdJbdc, credentialTypeIdJbdc, s"$api"))
-
-val jdbcCredentials = credentialResult.data
-match
+val token = sc.getConf.get("spark.nabu.token")
+val endpoint = sc.getConf.get("spark.nabu.fireshots_url")
+    
+val cred_id = args(0).toInt
+val cred_type = args(1).toInt
+  
+val CredentialResult = Credential.getCredentialData(CredentialPayload(s"$token", cred_id, cred_type, s"$endpoint"))
+  
+val ldap = CredentialResult.data match 
 {
-    case jdbcCredentials: jdbcCredentials => jdbcCredentials
-    case _ => throw new Exception("Currently unavailable for other credentials Types")
-}
+    case ldap: ldap => ldap
+    case _ => throw new Exception("Currently unable avalible for other credentials Types")
+  }
+  
+  val username = ldap.username
+  val password = ldap.password
   
 almaren.builder
-.sourceJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver","select * from mt3037.user",Some(jdbcCredentials.username), Some(jdbcCredentials.password))
-.targetJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver","mt3037.newtable",SaveMode.Overwrite,Some(jdbcCredentials.username), Some(jdbcCredentials.password))
+.sourceJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver","select * from mt3037.user",Some(username), Some(password))
+.targetJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver","mt3037.newtable",SaveMode.Overwrite,Some(username), Some(password))
 }
 match
 {
